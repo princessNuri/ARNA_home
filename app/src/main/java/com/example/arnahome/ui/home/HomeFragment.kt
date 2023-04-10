e63d832264c6588c789a002202929dc40a8deabc
princessnuri <nurgaeva1606@gmail.com>
2023-04-10 09:59:13 +0600

    Initial commit

---
+++ app/src/main/java/com/example/arnahome/ui/home/HomeFragment.kt
@@ -0,0 +1,19 @@
+package com.example.arnahome.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.arnahome.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+
+
+}
---
+++ app/src/main/java/com/example/arnahome/ui/onboard/OnBoardViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.arnahome.ui.onboard
+
+import androidx.lifecycle.ViewModel
+
+class OnBoardViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
